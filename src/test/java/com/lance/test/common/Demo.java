9f2f597f6ac3ad9a513898dffd1a798ec19adca3
Lance <634202275@qq.com>
2017-11-02 18:42:44 +0800

    java8 support code

--- src/test/java/com/lance/test/common/Demo.java
+++ src/test/java/com/lance/test/common/Demo.java
@@ -1,10 +1,19 @@
 package com.lance.test.common;
 
-import java.util.HashMap;
+
+import java.util.Comparator;
 
 public class Demo {
     public static void main(String[] args) {
-        HashMap map = new HashMap();
-        map.put("", "");
+        sop((a) -> a + "h");
     }
+
+    public static void sop(Printer p) {
+        System.out.println(p.print("hi"));
+    }
+}
+
+@FunctionalInterface
+interface Printer {
+    String print(String msg);
 }
