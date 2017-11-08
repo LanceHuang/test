7aa68b4b63cd5a8e660aa46abf2a0c305e18061c
Lance <634202275@qq.com>
2017-11-08 19:49:42 +0800

    Delete useless code

--- src/test/java/com/lance/test/common/Demo.java
+++ src/test/java/com/lance/test/common/Demo.java
@@ -1,19 +1,8 @@
 package com.lance.test.common;
 
-
-import java.util.Comparator;
-
 public class Demo {
+
     public static void main(String[] args) {
-        sop((a) -> a + "h");
     }
 
-    public static void sop(Printer p) {
-        System.out.println(p.print("hi"));
-    }
-}
-
-@FunctionalInterface
-interface Printer {
-    String print(String msg);
 }
