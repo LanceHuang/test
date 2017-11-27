43aaaa3ba2966612b8d23d54a02ca61486b90c2f
Lance <634202275@qq.com>
2017-11-27 14:44:27 +0800

    Delete useless code

--- src/test/java/com/lance/test/common/Demo.java
+++ src/test/java/com/lance/test/common/Demo.java
@@ -1,20 +1,6 @@
 package com.lance.test.common;
 
-import lombok.*;
-
-import static com.lance.common.tool.util.ConsolePrinter.*;
-
 public class Demo {
     public static void main(String[] args) {
-        Person p = new Person();
-        p.setName("Lance");
-        println(p);
     }
-}
-
-@Getter
-@Setter
-class Person {
-    private String name;
-    private int age;
 }
