6124f180aa07659884ae50cf80e32b1620820221
Lance <634202275@qq.com>
2017-12-01 09:51:56 +0800

    Delete useless code

--- src/main/java/com/lance/test/common/util/Sortable.java
+++
@@ -1,9 +0,0 @@
-package com.lance.test.common.util;
-
-/**
- * @author Lance
- * @since 2017/4/10
- */
-public interface Sortable {
-    <T extends Comparable> void sort(T[] arr);
-}
--- src/test/java/com/lance/test/common/Demo.java
+++ src/test/java/com/lance/test/common/Demo.java
@@ -1,5 +1,8 @@
 package com.lance.test.common;
 
+/**
+ * @author Lance
+ */
 public class Demo {
     public static void main(String[] args) {
     }
