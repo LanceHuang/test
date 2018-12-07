e94f83bb63462fb927346303622840e94b4f646e
634202275@qq.com <huanjmaxiada520>
2018-12-07 15:27:51 +0800

    Test ReentrantLock.lockInterruptibly

--- src/test/java/com/lance/test/common/concurrent/LockTest.java
+++ src/test/java/com/lance/test/common/concurrent/LockTest.java
@@ -3,7 +3,9 @@ package com.lance.test.common.concurrent;
 import org.junit.Test;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author Lance
@@ -45,8 +47,6 @@ public class LockTest {
 
     @Test
     public void testLockSupport() {
-
-
         Thread t1 = new Thread(() -> {
             System.out.println("t1 arrive");
             LockSupport.park(this);
@@ -65,10 +65,61 @@ public class LockTest {
         sleep(5);
     }
 
+    @Test
+    public void testLockInterruptibly() {
+        Lock lock = new ReentrantLock();
+
+        /*
+            Thread 2 arrive
+            Thread 1 arrive
+            Thread 2 do sth
+         */
+        Thread t1 = new Thread(() -> {
+            try {
+                System.out.println("Thread 1 arrive");
+//                lock.lock();
+                lock.lockInterruptibly();
+                System.out.println("Thread 1 do sth");
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                System.out.println("Thread 2 arrive");
+                lock.lock();
+                sleep(TimeUnit.MILLISECONDS, 15);
+                t1.interrupt();
+                System.out.println("Thread 2 do sth");
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        });
+
+        t2.start();
+        sleep(TimeUnit.MILLISECONDS, 10);
+        t1.start();
+
+        sleep(TimeUnit.SECONDS, 3);
+    }
+
 
     private void sleep(int second) {
         try {
             TimeUnit.SECONDS.sleep(second);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sleep(TimeUnit timeUnit, long t) {
+        try {
+            timeUnit.sleep(t);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
