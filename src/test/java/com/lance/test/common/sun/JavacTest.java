5848132b86f9829017e42df63395e212f5e33611
634202275@qq.com <huanjmaxiada520>
2020-01-09 15:06:07 +0800

    Manually compile java code

---
+++ src/test/java/com/lance/test/common/sun/JavacTest.java
@@ -0,0 +1,17 @@
+package com.lance.test.common.sun;
+
+import com.sun.tools.javac.Main;
+import org.junit.Test;
+
+/**
+ * @author Lance
+ * @since 2019/12/21 17:32
+ */
+public class JavacTest {
+
+    @Test
+    public void testCompile() {
+        String javaPath = "Demo.java";
+        Main.compile(new String[]{javaPath});
+    }
+}
