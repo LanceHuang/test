8ca6d213950f2705744975a6ad09fe8737e12755
634202275@qq.com <huanjmaxiada520>
2020-04-15 17:28:07 +0800

    Hibernate configuration

---
+++ src/test/java/com/lance/test/hibernate/User.java
@@ -0,0 +1,45 @@
+package com.lance.test.hibernate;
+
+/**
+ * @author Lance
+ * @since 2020/1/19 15:43
+ */
+public class User {
+
+    private int    id;
+    private String name;
+    private int    age;
+    private int    classId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+}
