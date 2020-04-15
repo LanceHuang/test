8fcc29df9f6f49e1d6684acc36904232d3641706
634202275@qq.com <huanjmaxiada520>
2020-04-15 18:33:32 +0800

    MongoDB Test

---
+++ src/test/java/com/lance/test/mongodb/MongoDBTest.java
@@ -0,0 +1,62 @@
+package com.lance.test.mongodb;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Lance
+ */
+public class MongoDBTest {
+
+    private MongoClient client;
+
+    @Before
+    public void before() {
+        client = MongoClients.create("mongodb://localhost:27017");
+    }
+
+    @After
+    public void after() {
+        client.close();
+    }
+
+    @Test
+    public void test() {
+        // C
+        MongoDatabase db = client.getDatabase("common");
+        MongoCollection<Document> coll = db.getCollection("test");
+        coll.insertOne(new Document().append("name", "Lance").append("age", 26));
+        coll.insertOne(new Document().append("name", "Alice").append("age", 25).append("message", "love"));
+        coll.insertOne(new Document().append("name", "Leo"));
+
+        // R
+        for (Document doc : coll.find()) {
+            System.out.println(doc);
+        }
+        System.out.println();
+
+        // U
+        coll.updateMany(Filters.eq("age", 26), new Document("$set", new Document("age", 24)));
+        for (Document doc : coll.find()) {
+            System.out.println(doc);
+        }
+        System.out.println();
+
+        // D
+        coll.deleteOne(Filters.eq("age", 24));
+        for (Document doc : coll.find()) {
+            System.out.println(doc);
+        }
+        System.out.println();
+
+        coll.drop();
+        db.drop();
+    }
+}
