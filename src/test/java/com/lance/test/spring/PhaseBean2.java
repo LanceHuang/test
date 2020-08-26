0557404410dce287a55e25bed11470b9fce68eb3
634202275@qq.com <huanjmaxiada520>
2020-08-26 20:55:01 +0800

    SmartLifecycle.getPhase()测试用例

---
+++ src/test/java/com/lance/test/spring/PhaseBean2.java
@@ -0,0 +1,41 @@
+package com.lance.test.spring;
+
+import org.springframework.context.SmartLifecycle;
+
+/**
+ * @author Lance
+ */
+public class PhaseBean2 implements SmartLifecycle {
+
+    private boolean running = false;
+
+    @Override
+    public void start() {
+        if (this.running) {
+            return;
+        }
+
+        System.out.println("PhaseBean2 start");
+        this.running = true;
+    }
+
+    @Override
+    public void stop() {
+        if (!this.running) {
+            return;
+        }
+
+        System.out.println("PhaseBean2 stop");
+        this.running = false;
+    }
+
+    @Override
+    public boolean isRunning() {
+        return this.running;
+    }
+
+    @Override
+    public int getPhase() {
+        return Integer.MIN_VALUE;
+    }
+}
---
+++ src/test/java/com/lance/test/spring/PhaseTest.java
@@ -0,0 +1,30 @@
+package com.lance.test.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Lance
+ */
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = PhaseTest.class)
+public class PhaseTest {
+
+    @Bean
+    public PhaseBean1 phaseBean1() {
+        return new PhaseBean1();
+    }
+
+    @Bean
+    public PhaseBean2 phaseBean2() {
+        return new PhaseBean2();
+    }
+
+    @Test
+    public void test() {
+        System.out.println("Hello world");
+    }
+}
