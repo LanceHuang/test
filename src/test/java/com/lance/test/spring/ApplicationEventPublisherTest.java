9798c51e569d657bde1b73c8ec109649ca09dc36
634202275@qq.com <huanjmaxiada520>
2020-08-27 12:49:39 +0800

    配置SimpleApplicationEventMulticaster

--- src/test/java/com/lance/test/spring/ApplicationEventPublisherTest.java
+++ src/test/java/com/lance/test/spring/ApplicationEventPublisherTest.java
@@ -3,7 +3,9 @@ package com.lance.test.spring;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -21,9 +23,21 @@ public class ApplicationEventPublisherTest {
     @Resource
     private ApplicationEventPublisher eventPublisher;
 
+    @Bean
+    public SimpleApplicationEventMulticaster applicationEventMulticaster() {
+        // 默认使用的也是SimpleApplicationEventMulticaster，但这里可以自定义一些配置
+        SimpleApplicationEventMulticaster multicaster = new SimpleApplicationEventMulticaster();
+        multicaster.setErrorHandler(new DefaultErrorHandler()); // 自定义异常处理器
+//        multicaster.setTaskExecutor(); // 自定义任务执行器，可以让任务异步进行（变成异步事件）
+        // 又或者自定义一个AbstractApplicationEventMulticaster，里面套两个SimpleApplicationEventMulticaster，其中一个TaskExecutor
+        // 从而同时支持同步/异步事件
+        return multicaster;
+    }
+
     @EventListener
     public void testEvent(TestEvent event) { // 接收并处理事件，同步
         System.out.println("Receive TestEvent");
+//        System.out.println(799/0); // 用于测试ErrorHandler
     }
 
     @Test
---
+++ src/test/java/com/lance/test/spring/DefaultErrorHandler.java
@@ -0,0 +1,18 @@
+package com.lance.test.spring;
+
+import org.springframework.util.ErrorHandler;
+
+/**
+ * 默认异常处理器
+ *
+ * @author Lance
+ */
+public class DefaultErrorHandler implements ErrorHandler {
+
+    @Override
+    public void handleError(Throwable t) {
+        // 异常处理
+        System.out.println("报错啦");
+        t.printStackTrace();
+    }
+}
