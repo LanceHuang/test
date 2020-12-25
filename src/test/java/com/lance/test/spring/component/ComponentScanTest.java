2e4153e1d1816b69dcb82fd54451698fe4fc02e5
634202275@qq.com <huanjmaxiada520>
2020-12-25 12:28:26 +0800

    删除冗余代码

---
+++ src/test/java/com/lance/test/spring/component/ComponentScanTest.java
@@ -0,0 +1,27 @@
+package com.lance.test.spring.component;
+
+import com.lance.test.spring.component.service.IComponentScanTestService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Lance
+ * @since 2020/12/25
+ */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = ComponentScanTest.class)
+public class ComponentScanTest {
+
+    @Resource
+    private IComponentScanTestService testService;
+
+    @Test
+    public void test() {
+        System.out.println(testService);
+    }
+
+}
---
+++ src/test/java/com/lance/test/spring/component/service/ComponentScanTestService.java
@@ -0,0 +1,8 @@
+package com.lance.test.spring.component.service;
+
+/**
+ * @author Lance
+ * @since 2020/12/25
+ */
+public class ComponentScanTestService implements IComponentScanTestService {
+}
