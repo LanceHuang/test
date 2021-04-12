be59177b62163212e3edb9d32f34cb816f724b96
634202275@qq.com <huanjmaxiada520>
2021-04-12 16:49:58 +0800

    JedisPool单元测试

--- src/test/java/com/lance/test/redis/RedisTest.java
+++ src/test/java/com/lance/test/redis/RedisTest.java
@@ -1,35 +1,36 @@
 package com.lance.test.redis;
 
-import com.lance.test.log.LoggerUtil;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisTest {
-    private Jedis redisClient;
-
-    @Before
-    public void before() {
-        redisClient = new Jedis();
-        LoggerUtil.info("Before");
-    }
-
-    @After
-    public void after() {
-        redisClient.close();
-        System.out.println("After");
-    }
 
     @Test
-    public void testSet() {
+    public void testJedis() {
+        Jedis redisClient = new Jedis();
         String resultCode = redisClient.set("name", "Lance");
         System.out.println(resultCode);
+        String name = redisClient.get("name");
+        System.out.println(name);
+        redisClient.close();
     }
 
     @Test
-    public void testGet() {
-        String name = redisClient.get("name");
+    public void test() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMinIdle(50);
+        jedisPoolConfig.setMaxIdle(200);
+        jedisPoolConfig.setMaxWaitMillis(100 * 1000L);
+
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, "127.0.0.1", 6379);
+        Jedis jedis = jedisPool.getResource();
+        String resultCode = jedis.set("name", "Lance");
+        System.out.println(resultCode);
+        String name = jedis.get("name");
         System.out.println(name);
+        jedis.close();
+        jedisPool.close();
     }
 }
