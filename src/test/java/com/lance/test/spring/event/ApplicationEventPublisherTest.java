af20760cf3df7cf8abfe06369dd1cdec0e0fb6b2
634202275@qq.com <huanjmaxiada520>
2021-07-09 10:00:17 +0800

    用junit5测试spring事件

--- src/test/java/com/lance/test/spring/event/ApplicationEventPublisherTest.java
+++ src/test/java/com/lance/test/spring/event/ApplicationEventPublisherTest.java
@@ -1,13 +1,14 @@
 package com.lance.test.spring.event;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import javax.annotation.Resource;
 
@@ -16,33 +17,48 @@ import javax.annotation.Resource;
  *
  * @author Lance
  */
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = ApplicationEventPublisherTest.class)
+@SpringBootTest(classes = ApplicationEventPublisherTest.Config.class)
 public class ApplicationEventPublisherTest {
 
     @Resource
     private ApplicationEventPublisher eventPublisher;
 
-    @Bean
-    public SimpleApplicationEventMulticaster applicationEventMulticaster() {
-        // 默认使用的也是SimpleApplicationEventMulticaster，但这里可以自定义一些配置
-        SimpleApplicationEventMulticaster multicaster = new SimpleApplicationEventMulticaster();
-        multicaster.setErrorHandler(new DefaultErrorHandler()); // 自定义异常处理器
-//        multicaster.setTaskExecutor(); // 自定义任务执行器，可以让任务异步进行（变成异步事件）
-        // 又或者自定义一个AbstractApplicationEventMulticaster，里面套两个SimpleApplicationEventMulticaster，其中一个TaskExecutor
-        // 从而同时支持同步/异步事件
-        return multicaster;
-    }
-
-    @EventListener
-    public void onTestEvent(TestEvent event) { // 接收并处理事件，同步
-        System.out.println("Receive TestEvent");
-//        System.out.println(799/0); // 用于测试ErrorHandler
-    }
-
     @Test
     public void test() {
-        eventPublisher.publishEvent(new TestEvent()); // 发布事件
-        System.out.println("Hello world");
+        System.out.println("Publisher thread: " + Thread.currentThread());
+        eventPublisher.publishEvent(new TestEvent());
+    }
+
+    @SpringBootApplication
+    @EnableAsync
+    public static class Config {
+
+        @EventListener
+        public void syncEvent1(TestEvent event) {
+            System.out.println("syncEvent1 thread: " + Thread.currentThread());
+        }
+
+        @EventListener
+        public void syncEvent2(TestEvent event) {
+            System.out.println("syncEvent2 thread: " + Thread.currentThread());
+        }
+
+        @EventListener
+        @Async
+        public void asyncEvent(TestEvent event) {
+            System.out.println("asyncEvent thread: " + Thread.currentThread());
+        }
+
+        @Bean
+        public SimpleApplicationEventMulticaster applicationEventMulticaster() {
+            // 默认使用的也是SimpleApplicationEventMulticaster，但这里可以
+            // 自定义一些配置
+            SimpleApplicationEventMulticaster multicaster = new SimpleApplicationEventMulticaster();
+            multicaster.setErrorHandler(new DefaultErrorHandler()); // 自定义异常处理器
+//        multicaster.setTaskExecutor(); // 自定义任务执行器，可以让任务异步进行（变成异步事件）
+            // 又或者自定义一个AbstractApplicationEventMulticaster，里面套两个SimpleApplicationEventMulticaster，其中一个TaskExecutor
+            // 从而同时支持同步/异步事件
+            return multicaster;
+        }
     }
 }
