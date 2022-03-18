e10515c7449af318164dac8a8c120bf71c130f3d
634202275@qq.com <huanjmaxiada520>
2022-03-18 14:38:48 +0800

    LockSupport单元测试

--- src/test/java/com/lance/test/common/concurrent/LockSupportTest.java
+++ src/test/java/com/lance/test/common/concurrent/LockSupportTest.java
@@ -1,11 +1,14 @@
 package com.lance.test.common.concurrent;
 
+import com.lance.common.tool.ThreadUtils;
 import org.junit.Test;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
 /**
+ * 类似于Object.wait，但不需要放在synchronized中
+ *
  * @author Lance
  * @since 2018/11/15 21:46
  */
@@ -13,50 +16,24 @@ public class LockSupportTest {
 
     @Test
     public void test() {
-        System.out.println("Ready...");
-
         final Thread mainThread = Thread.currentThread();
-
         new Thread(() -> {
-            try {
-                TimeUnit.SECONDS.sleep(5);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            // 子线程休眠1s
+            ThreadUtils.sleep(1000L);
+            // 子线程执行
+            System.out.println("子线程执行");
 
+            // 唤醒主线程
+            System.out.println("唤醒主线程");
             LockSupport.unpark(mainThread);
         }).start();
 
+        // 挂起主线程
+        System.out.println("挂起主线程");
+//        LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(500L));
+        LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(1500L));
 
-        LockSupport.park();
-        System.out.println("Finished...");
-    }
-
-    @Test
-    public void testParkNanos() {
-        System.out.println("Ready...");
-
-        final Thread mainThread = Thread.currentThread();
-
-        new Thread(() -> {
-            try {
-                TimeUnit.SECONDS.sleep(5);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Fi");
-
-            LockSupport.unpark(mainThread);
-        }).start();
-
-
-        LockSupport.parkNanos(2000000000L);
-        System.out.println("Finished...");
-
-        try {
-            TimeUnit.SECONDS.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        // 主线程唤醒后执行
+        System.out.println("主线程执行");
     }
 }
